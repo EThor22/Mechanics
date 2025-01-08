70d1c504696d1a48a37c627750d58f82a4166786
Evan <evthor007@icloud.com>
2025-01-08 14:11:37 -0500

    Done 4.2

--- Mechanics/Assets/Scripts/PlayerController.cs
+++ Mechanics/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalpoint;
-    public float speed = 5.0f;
+    public float speed = 3.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
